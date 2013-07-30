5842b49f3547f81d42fc6b0e89f9453fb675b422
Patrick <p.crager@gmail.com>
2013-07-29 22:35:13 -0400

    New model for V2 deals

--- api/Models/DealV2.vb
+++ api/Models/DealV2.vb
@@ -6,13 +6,17 @@
     ''' <remarks></remarks>
     Public Class Deal
 
+        Public Property slug As String
         Public Property title As String
+        Public Property text As String
+        Public Property sTitle As String
+        Public Property author As String
+        Public Property _id As String
+        Public Property legacy As Boolean
+        Public Property images As List(Of String)
+        Public Property hot As Boolean
         Public Property expired As Boolean
-        Public Property timestamp As String
-        Public Property [date] As String
-        Public Property link As String
-        Public Property body As String
-        Public Property image As String
+        Public Property dealDate As DateTime
 
     End Class
 
@@ -22,8 +26,13 @@
     ''' <remarks></remarks>
     Public Class RootDeal
 
-        Public Property numDeals As Integer
-        Public Property deals As List(Of Deal)
+        Public Property pages As Integer
+        Public Property page As Integer
+        Public Property count As Integer
+        Public Property query As String
+        Public Property data As Dictionary(Of String, List(Of Deal))
+        Public Property view As String
+        Public Property showExpired As Boolean
 
     End Class
 
